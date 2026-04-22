9d78033ead4777821c307ada8f87fc68b6e5bc47
jballesterosc <jayballesterosc@gmail.com>
2026-04-22 15:41:57 -0500

    DAP W5

--- 3_spring_2026/data_programming/skills-psets/W5.py
+++ 3_spring_2026/data_programming/skills-psets/W5.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 # 1.1.
@@ -21,3 +22,29 @@ df_iris.shape
 # 1.3. 
 
 df_iris['species'].unique()
+df_iris['species'].value_counts()
+
+
+# 1.4. 
+# df_iris.mean() this prints error, since it cannot calculate species (object dtype) 
+
+np.mean(df_iris.select_dtypes(include=["float64"]))
+
+df_iris.select_dtypes(include=["float64"]).mean()
+df_iris.select_dtypes(include=["float64"]).median()
+df_iris.select_dtypes(include=["float64"]).std()
+
+
+df_iris.set_index('species')
+
+# 1.5.
+df_iris.describe()
+
+
+# 1.6. Create a new DataFrame by subsetting only the petal columns using a list comprehension. Do this 
+# three ways - one that creates a view, and two that create new copies.
+
+petals = [p for p in df_iris.columns if p.startswith('petal')]
+df_petals = df_iris[petals]
+df_petals
+
